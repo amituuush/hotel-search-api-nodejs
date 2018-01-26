8fd4faa219f309a6429db7de813f17ce160ff3e1
Amit Ranan <ranan.amit@gmail.com>
2018-01-25 21:35:36 -0800

    iterating through data, closure issue

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
+const rp = require('request-promise');
+
+function setOptionsUri(provider) {
+  var options = {
+    uri: `http://localhost:9000/scrapers/${provider}`,
+    headers: { 'User-Agent': 'Request-Promise' },
+    json: true,
+  };
+  return options;
+}
+
+hotelProviders = ['expedia', 'orbitz', 'priceline', 'travelocity', 'hilton'];
 
 app.get('/hotels/search', function(req, res) {
-  axios
-    .get('http://localhost:9000/scrapers/expedia')
-    .then(function(response) {
-      console.log(response.data);
-      res.send(response.data);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+  let resultsSum = { results: [] };
+  results = [];
+
+  hotelProviders.forEach(provider => {
+    rp(setOptionsUri(provider))
+      .then(function(response) {
+        response.data.results.forEach(result => {
+          results.push(result);
+          console.log('result', results.length);
+        });
+        // console.log(results);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  });
+  console.log('results', results);
+  res.send(results);
 });
 
 app.listen(8000, function() {
